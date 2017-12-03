9cfd505015744c4bd9755303457ba5b259b6832c
xiandafu <xiandafu@126.com>
2017-12-03 12:57:55 +0800

    test

--- 9_test/ch9.test/src/test/java/com/bee/sample/ch9/test/service/UserServiceTest.java
+++ 9_test/ch9.test/src/test/java/com/bee/sample/ch9/test/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
