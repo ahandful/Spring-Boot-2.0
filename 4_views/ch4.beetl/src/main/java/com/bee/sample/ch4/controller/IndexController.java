59f661e3eb1315c41b0c6ebe5eecf47c70f35bcc
xiandafu <xiandafu@126.com>
2018-07-10 00:05:30 +0800

    refine doc

--- 4_views/ch4.beetl/src/main/java/com/bee/sample/ch4/controller/IndexController.java
+++ 4_views/ch4.beetl/src/main/java/com/bee/sample/ch4/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.bee.sample.ch4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * 学习beetl语法，可以参考在线体验:http://ibeetl.com/beetlonline/,学习大部分语法
  * @author xiandafu
@@ -10,6 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class IndexController {
+	
+	@Autowired
+	ObjectMapper objectMapper;
 	@RequestMapping("/index.do")
 	public ModelAndView  index(){
 		ModelAndView view = new ModelAndView("/index.btl");
