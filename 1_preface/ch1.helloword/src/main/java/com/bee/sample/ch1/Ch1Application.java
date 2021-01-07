a8f5777fd9409d02ac035bbd6e9dae75d6949594
ahandful <34510891+ahandful@users.noreply.github.com>
2021-01-07 11:51:09 +0800

    Update Ch1Application.java

--- 1_preface/ch1.helloword/src/main/java/com/bee/sample/ch1/Ch1Application.java
+++ 1_preface/ch1.helloword/src/main/java/com/bee/sample/ch1/Ch1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ch1Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Ch1Application.class, args);
 
